0953d0fe17186bd15510c8fcf83ecb6914e3c985
Young Sam Concepts <youngsamconcepts@gmail.com>
2024-04-24 20:52:32 +0000

    My sub initial commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,14 +19,18 @@ function userScroll() {
   const navbar = document.querySelector('.navbar');
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
-      navbar.classList.add('bg-dark');
+    if (window.innerWidth >= 992) {
+    	if (window.scrollY >= 10) {
+      	navbar.classList.add('bg-dark');
+      } else {
+	      navbar.classList.remove('bg-dark');
+	    }
     } else {
-      navbar.classList.remove('bg-dark');
+      navbar.classList.add('bg-dark');
     }
   });
   window.addEventListener('DOMContentLoaded', () => {
-    if (window.scrollY > 10) {
+    if (window.scrollY > 10 && window.innerWidth) {
       navbar.classList.add('bg-dark');
     } else {
       navbar.classList.remove('bg-dark');
@@ -35,17 +39,17 @@ function userScroll() {
 }
 document.addEventListener('DOMContentLoaded', userScroll);
 /*
-	NAV LOGO
-	********
+	NAV LOGO TYPING TEXT
+	********************
 */
-function runTypingEffect() {
+function logoTypingEffect() {
   const text = 'Young Sam Concepts';
   const typingElement = document.getElementById('typing-text');
   const typingDelay = 100;
 
   typeText(text, typingElement, typingDelay);
 }
-document.addEventListener('DOMContentLoaded', runTypingEffect);
+document.addEventListener('DOMContentLoaded', logoTypingEffect);
 /*
 	BACK TO TOP
 	***********
@@ -58,3 +62,18 @@ function backToTop() {
 	});
 }
 
+
+function openExternalLinks(linkElements) {
+	const externalLinks = document.querySelectorAll('#external-link');
+	const externalLinksFrame = document.querySelector('#external-link-frame iframe');
+	const externalLinksPageTitle = document.querySelector('#external-link-frame #site-title');
+
+	externalLinks.forEach(link => {
+		link.addEventListener('click', e => {
+			externalLinksFrame.src = link.href;
+			externalLinksPageTitle.innerHTML = link.href;
+		});
+	});
+	
+}
+document.addEventListener('DOMContentLoaded', openExternalLinks);
