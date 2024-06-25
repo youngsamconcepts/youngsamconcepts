582b3f48bc447bc7c08a305a9874d0788fc100ab
Young Sam Concepts <youngsamconcepts@gmail.com>
2024-06-25 05:30:13 +0000

    Done with the  service page

--- assets/js/contact-msg.js
+++ assets/js/contact-msg.js
@@ -18,11 +18,11 @@ function sendMSG() {
 		if (message !== 'OK') {
       const text = 'Message not sent! Something went wrong, please try again!'
       $('#feedback').addClass('alert-warning').text(text).fadeIn('slow');
-      $('#feedback').fadeOut(5000);
+      $('#feedback').fadeOut(7000);
     }
     const text = 'Message successfully sent! Thanks for your message, we will review your message and get back to you as soon as possible using the email you provided.'
     $('#feedback').text(text).addClass('alert-success').fadeIn('slow');
-    $('#feedback').fadeOut(5000);
+    $('#feedback').fadeOut(7000);
 	});
 }
 // Validate the contact form and send
